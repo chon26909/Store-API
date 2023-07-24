31ead9a34ddae6d4d504ac01165eebb949adbf78
HiChon <59905927+chon26909@users.noreply.github.com>
2023-07-24 21:56:59 +0700

    update user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,11 @@
 import console from 'console';
 import { Request, Response } from 'express';
-import { db } from '../config/database';
+// import { db } from '../config/database';
 import { generatePassword, generateSalt } from '../helper/auth';
-import { getUsersData, setUsersData } from '../redis/user';
-import { IUser, ROLES_USER, USER_STATUS } from '../types/user';
+import { getUsersData } from '../redis/user';
+import { ROLES_USER, USER_STATUS } from '../types/user';
+import { v4 as uuidv4 } from 'uuid';
+import { addUser } from '../database/user';
 
 export const createUser = async (req: Request, res: Response) => {
     const body = req.body;
@@ -12,15 +14,37 @@ export const createUser = async (req: Request, res: Response) => {
     const date = new Date();
     const user_status = USER_STATUS.ENABLED;
     const role = ROLES_USER.ADMIN;
+    const id = uuidv4();
 
-    const q = ` INSERT INTO users (firstname, lastname, email, password, salt, user_status, role, create_at, update_at) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-    db.query(q, [body.firstname, body.lastname, body.email, hashPassword, salt, user_status, role, date, date], (err) => {
-        if (err) {
-            res.status(500).json({ message: 'server error' });
+    try {
+        const data = {
+            id: id,
+            firstname: body.firstname,
+            lastname: body.lastname,
+            email: body.email,
+            hash: hashPassword,
+            salt: salt,
+            user_status: user_status,
+            role: role,
+            created_at: date,
+            updated_at: date
+        };
+
+        const created = await addUser(data);
+        if (created) {
+            res.status(201).json({ message: 'created' });
         }
-    });
-    res.status(201).json({ message: 'created' });
+    } catch (error) {
+        console.log('error', error);
+    }
+
+    // const q = ` INSERT INTO users (id, firstname, lastname, email, password, salt, user_status, role, create_at, update_at)
+    //             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    // db.query(q, [id, body.firstname, body.lastname, body.email, hashPassword, salt, user_status, role, date, date], (err) => {
+    //     if (err) {
+    //         res.status(500).json({ message: 'server error' });
+    //     }
+    // });
 };
 
 export const getUsers = async (req: Request, res: Response) => {
