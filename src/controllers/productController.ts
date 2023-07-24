29ab36ddacd5e635395e77cd95079cbfbf15581f
HiChon <59905927+chon26909@users.noreply.github.com>
2023-07-24 21:56:52 +0700

    update product controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,45 +1,54 @@
 import { Request, Response } from 'express';
 import { db } from '../config/database';
 import { uploadImage } from '../helper/multer';
+import { addProduct, getProductID, getProductList } from '../database/product';
 
-export const getProducts = (req: Request, res: Response) => {
-    const query = 'SELECT * FROM products';
-    db.query(query, (err, rows: []) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ message: 'Server error' });
-        }
-        const data = rows.map((row: any) => ({
-            id: row.id,
-            name: row.name,
-            description: row.description,
-            price: row.price,
-            qty: row.qty,
-            picture: process.env.CLOUDINARY_PUBLIC_URL + row.picture
-        }));
-        res.status(201).json({ message: 'success', data });
-    });
-};
-export const createProduct = async (req: Request, res: Response) => {
-    const data = req.body;
-    console.log('data', data);
-    const date_now = new Date();
-    const create_by = '1';
-
+export const getProducts = async (req: Request, res: Response) => {
     try {
-        const { secure_url } = await uploadImage(data.picture);
-        const picture_name = secure_url.split('/').pop();
-        const q = ` INSERT INTO products (name, description, price, qty, picture, create_by, create_at) 
-                VALUES (?,?,?,?,?,?,?)`;
-        db.query(q, [data.name, data.description, data.price, data.qty, picture_name, create_by, date_now], (err) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({ message: 'Server error' });
-            }
-        });
+        const productList = await getProductList();
+
+        console.log('productList: ' + productList);
+
+        res.status(201).json({ message: 'success', data: productList });
     } catch (error) {
         console.log(error);
     }
+};
 
-    res.status(201).json({ message: 'created' });
+export const getProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const product = await getProductID(id);
+    } catch (error) {}
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+    const body = req.body;
+    // console.log('body', body);
+    const date_now = new Date();
+    const created_by = '1';
+
+    try {
+        // const { secure_url } = await uploadImage(body.image);
+        // const image_name = secure_url.split('/').pop();
+
+        const data = {
+            name: body.name,
+            description: body.description,
+            price: body.price,
+            qty: body.qty,
+            image: '' as string,
+            created_at: date_now,
+            created_by: created_by,
+            updated_at: date_now,
+            updated_by: created_by
+        };
+        const created = await addProduct(data);
+        if (created) {
+            res.status(201).json({ message: 'created' });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
