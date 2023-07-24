e8dc111749989c58c882908eac3147ff0e679893
HiChon <59905927+chon26909@users.noreply.github.com>
2023-07-24 21:57:21 +0700

    update

--- src/database/user.ts
+++ src/database/user.ts
@@ -1,3 +1,31 @@
-const getUsers = () => {
-        
+import { OkPacket } from 'mysql2';
+import { db } from '../config/database';
+
+interface IAddUser {
+    id: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    hash: string;
+    salt: string;
+    user_status: string;
+    role: string;
+    created_at: Date;
+    updated_at: Date;
 }
+
+export const addUser = (data: IAddUser) => {
+    return new Promise<boolean>((resolve, reject) => {
+        const q = ` INSERT INTO users (id, firstname, lastname, email, password, salt, user_status, role, created_at, updated_at) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        db.query<OkPacket>(q, [data.id, data.firstname, data.lastname, data.email, data.hash, data.salt, data.user_status, data.role, data.created_at, data.updated_at], (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (result.insertId) {
+                    resolve(true);
+                }
+            }
+        });
+    });
+};
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
-import { db } from '../config/database';
-import { createProduct, getProducts } from '../controllers/productController';
+import { createProduct, getProducts, getProduct } from '../controllers/productController';
 import { handleAuthentication } from '../middleware/verifyHeader';
 
 const router: Router = express.Router();
 
 router.use(handleAuthentication);
 router.get('/', getProducts);
+router.get('/:id', getProduct);
 router.post('/create', createProduct);
 
 export default router;
