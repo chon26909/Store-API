7ed6be2b87be4a9b147459bd81de3e146cb78f36
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-11 20:10:55 +0700

    handle login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,21 +1,31 @@
 import { Request, Response } from 'express';
-import { db } from '../config/database';
 import { IAuth } from '../types/auth';
+import { db } from '../database/mysql';
 
 export const handleLogin = (req: Request, res: Response) => {
     console.log('login');
 
-    const { email, password }: IAuth = req.body;
+    // db.query('SELECT * FROM users', (err, result) => {
+    //     if (err) {
+    //         console.log('err', err);
+    //     } else {
+    //         console.log('result', result);
+    //     }
+    // });
 
-    const q = 'SELECT email, password, salt FROM users WHERE email = ?';
+    // getAllUser()
 
-    db.query(q, [email], (err, data) => {
-        if (err) {
-            console.log('err', err);
-        }
-        // return res.status(400).json({ message: 'email or password not found' });
-        if (data) {
-            res.status(200).json({ message: 'success', token: 'rweihvuberugheht[5tyu8hjiuhjioj' });
-        }
-    });
+    // const { email, password }: IAuth = req.body;
+
+    // const q = 'SELECT email, password, salt FROM users WHERE email = ?';
+
+    // db.query(q, [email], (err, data) => {
+    //     if (err) {
+    //         console.log('err', err);
+    //     }
+    //     // return res.status(400).json({ message: 'email or password not found' });
+    //     if (data) {
+    //         res.status(200).json({ message: 'success', token: 'rweihvuberugheht[5tyu8hjiuhjioj' });
+    //     }
+    // });
 };
