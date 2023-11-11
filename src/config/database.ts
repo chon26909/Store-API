c943ef194b299601df504f746a80d18dec5692f8
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-11 20:10:40 +0700

    create connection database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,11 @@
-import mysql from "mysql2";
-import * as dotenv from "dotenv";
+import mysql from 'mysql2';
+import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : `.env` });
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
